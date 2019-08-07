417eea820d7b22ce8d2b5252c8e06999765933b2
Christopher Wright <cwright814@gmail.com>
2019-08-07 17:27:59 -0400

    Implement custom navbar component

--- src/App.vue
+++ src/App.vue
@@ -1,30 +1,31 @@
 <template>
 <div id="app">
-    <div id="nav">
-        <router-link to="/">Home</router-link> |
-        <router-link to="/api">API Example</router-link> |
-        <router-link to="/about">About</router-link>
-    </div>
+    <NavBar/>
     <router-view/>
 </div>
 </template>
+
+<script lang="ts">
+import { Component, Vue } from "vue-property-decorator";
+import NavBar from "@/components/NavBar.vue"; // @ is an alias to /src
+
+@Component({
+    components: {
+        NavBar
+    }
+})
+export default class App extends Vue {}
+</script>
 
 <style lang="scss">
 #app {
     font-family: "Avenir", Helvetica, Arial, sans-serif;
     -webkit-font-smoothing: antialiased;
     -moz-osx-font-smoothing: grayscale;
-    text-align: center;
     color: #2c3e50;
-}
-#nav {
-    padding: 30px;
-    a {
-        font-weight: bold;
-        color: #2c3e50;
-        &.router-link-exact-active {
-            color: #42b983;
-        }
+    &:nth-child(2) {
+        text-align: center;
+        margin-top: 4rem;
     }
 }
 </style>
---
+++ src/components/NavBar.vue
@@ -0,0 +1,71 @@
+<template>
+<b-navbar toggleable="sm" type="light" variant="info" fixed="top">
+	<b-navbar-brand>Portfolio 2019</b-navbar-brand>
+
+	<b-navbar-toggle target="nav-collapse"></b-navbar-toggle>
+
+	<b-collapse id="nav-collapse" is-nav>
+		<b-navbar-nav id="nav">
+			<b-nav-item :to="{ name: 'intro' }">Intro</b-nav-item>
+			<b-nav-item :to="{ name: 'experience' }">Experience</b-nav-item>
+			<b-nav-item :to="{ name: 'examples' }">Examples</b-nav-item>
+		</b-navbar-nav>
+	</b-collapse>
+</b-navbar>
+</template>
+
+<style scoped lang="scss">
+nav {
+	background-color: #ffffff !important;
+	border-bottom: 1px solid #eaecef;
+}
+#nav {
+	a {
+		font-size: 1.25rem;
+		color: #2c3e50;
+		@media (max-width: 575px) {
+			&:hover, &.router-link-exact-active {
+				color: #46bd87 !important;
+			}
+		}
+	}
+	.nav-link {
+		padding-bottom: 0;
+	}
+}
+@media (min-width: 576px) {
+	#nav {
+		margin-left: auto;
+		margin-right: auto;
+		a {
+			&.router-link-exact-active {
+				border-bottom-color: #46bd87;
+				border-bottom-style: solid;
+				border-bottom-width: 2px;
+			}
+			&:hover {
+				border-bottom-color: #46bd87;
+				border-bottom-style: solid;
+				border-bottom-width: 2px;
+			}
+		}
+	}
+	#nav-collapse {
+		// Force full width regardless of branding
+		position: absolute;
+		width: 100%;
+		margin-left: -16px;
+	}
+	.navbar-expand-sm .navbar-nav .nav-link {
+		padding: 0;
+		margin-left: 0.5rem;
+		margin-right: 0.5rem;
+	}
+}
+.navbar {
+	padding: 0.5rem 1rem;
+}
+.navbar-brand {
+	color: #2c3e50;
+}
+</style>
